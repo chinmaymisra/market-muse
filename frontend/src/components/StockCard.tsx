7e2eba094ef5df4b750e1bdde9bd7b29eac16397
Chinmay Misra <misrachinmay@gmail.com>
2025-05-08 03:41:38 +0530

    Fixes

--- frontend/src/components/StockCard.tsx
+++ frontend/src/components/StockCard.tsx
@@ -1,5 +1,5 @@
 import { Stock } from "../types";
-import { LineChart, Line, ResponsiveContainer } from "recharts";
+import { LineChart, Line, ResponsiveContainer, XAxis, YAxis } from "recharts";
 import { useEffect, useState } from "react";
 
 interface Props {
